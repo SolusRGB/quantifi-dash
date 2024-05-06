3190d1f60a73ba5508679841a5f48fee6656637a
SolusRGB <brandoneddycode@gmail.com>
2024-05-06 15:59:23 +0100

    Add support for using images from Discord's CDN in Next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,11 +1,25 @@
 await import("./src/env.js");
 
+/**
+ * This is used for the Next.js configuration.
+ * I have added line 16-22 to enable the ability to use images from Discord's CDN. (For profile pics)
+ */
+
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: false,
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.discordapp.com",
+        port: "",
+      },
+    ],
   },
 };
 
