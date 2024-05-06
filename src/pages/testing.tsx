22c27628ad4453e899382cbddb84b6c531c13c4b
SolusRGB <brandoneddycode@gmail.com>
2024-05-06 16:01:25 +0100

    Add testing page to the application

---
+++ src/pages/testing.tsx
@@ -0,0 +1,17 @@
+import HomeTableTest from "@/components/tables/testing/HomeTableTest";
+
+/**
+ * This is the testing page for the application.
+ * This was created to test the functionality of adding projects to the database
+ * Then routing it into the front end of the application.
+ * @returns {JSX.Element} - The testing page.
+ */
+
+export default function testing() {
+  return (
+    <div>
+      testing..
+      <HomeTableTest />;
+    </div>
+  );
+}
