ef6c5667cbaa134ab90825d94a930262aba9e1bd
SolusRGB <brandoneddycode@gmail.com>
2024-05-06 16:01:02 +0100

    Refactor HomeTable component to fetch latest projects from API

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,17 @@
-import { HomeTable } from "@/components/tables/home/HomeTable";
+import HomeTable from "@/components/tables/home/HomeTable";
 import { FeaturedSection } from "../components/cards/home/FeaturedSection";
 import HighlightedProject from "../components/cards/home/HighlightedProject";
 
 import { IndexLayout } from "../layout/Layout";
 import News from "@/components/trading-view/charts/news";
+
+/**
+ * This is the home page for the application.
+ * It is displayed when a user navigates to the root URL.
+ * It includes the HighlightedProject, FeaturedSection, News, and HomeTable components.
+ * This was created to get the root URL to work correctly. (index.tsx)
+ * @returns {JSX.Element} - The home page.
+ */
 
 export default function Dashboard() {
   return (
