9712975367e1f2783898d2cd5023646385e3e142
SolusRGB <brandoneddycode@gmail.com>
2024-05-06 16:01:32 +0100

    Update authentication options in [...nextauth].ts

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,4 +2,10 @@ import NextAuth from "next-auth";
 
 import { authOptions } from "@/server/auth";
 
+/**
+ * This file exports the default NextAuth instance with the provided authentication options.
+ *
+ * @param {object} authOptions - The authentication options for NextAuth.
+ * @returns {object} - The NextAuth instance.
+ */
 export default NextAuth(authOptions);
