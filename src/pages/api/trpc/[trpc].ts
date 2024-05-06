a3f013ae8b6dc35905a86e652a228dd6e69ed29b
SolusRGB <brandoneddycode@gmail.com>
2024-05-06 16:01:48 +0100

    Refactor tRPC API handler configuration in [trpc].ts

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,19 +1,22 @@
 import { createNextApiHandler } from "@trpc/server/adapters/next";
-
 import { env } from "@/env";
 import { appRouter } from "@/server/api/root";
 import { createTRPCContext } from "@/server/api/trpc";
 
-// export API handler
+// Create and export the tRPC API handler
 export default createNextApiHandler({
+  // Specify the app router
   router: appRouter,
+  // Specify the function to create the tRPC context
   createContext: createTRPCContext,
+  // Specify the error handling behavior based on the environment
   onError:
     env.NODE_ENV === "development"
       ? ({ path, error }) => {
+          // In development environment, log the error with the path and error message
           console.error(
             `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
           );
         }
-      : undefined,
+      : undefined, // In production environment, don't log errors
 });
